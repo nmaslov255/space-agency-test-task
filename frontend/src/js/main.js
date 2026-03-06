05cfb0b4a186a2caa3bce6bdb549131ee485ab31
Nikolay <nikolay.maslov@arora.pro>
2026-03-06 17:47:37 +0300

    added slider on bottom section of layout

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,8 +1,50 @@
 import '../css/styles.scss';
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import $ from 'jquery';
 
-window.$ = $;
-window.jQuery = $;
+import $ from 'jquery'
 
+window.$ = $
+window.jQuery = $
+
+import 'slick-carousel'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
+
+  $(document).ready(function() {
+    $('.slider-main').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      fade: true,
+      infinite: true,
+      asNavFor: '.slider-nav',
+      initialSlide: 3,
+    })
+
+  $('.slider-nav').slick({
+    slidesToShow: 5,
+    arrows: true,
+    prevArrow: '<button class="slick-prev">&#8592;</button>',
+    nextArrow: '<button class="slick-next">&#8594;</button>',
+    slidesToScroll: 1,
+    asNavFor: '.slider-main',
+    focusOnSelect: true,
+    infinite: true,
+    initialSlide: 3,
+    centerMode: true,
+    centerPadding: '0px',
+
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          arrows: false,
+          centerPadding: null,
+        }
+      },
+    ]
+  })
+})
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
+
 export default defineConfig({
   root: resolve(__dirname, 'src'),
   build: {
