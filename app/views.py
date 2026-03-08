94cfdc65e1d73dba47ace4ecb36e47c8855945f1
Nikolay <nikolay.maslov@arora.pro>
2026-03-08 10:47:50 +0300

    added noimage in slider

--- app/views.py
+++ app/views.py
@@ -1,6 +1,14 @@
+from django.conf import settings
 from django.shortcuts import render
 from slider.models import SliderImage
+from types import SimpleNamespace as SN
 
 def index(request):
-    images = SliderImage.objects.all()
+    images = list(SliderImage.objects.all())
+
+    # для корректной версти клайдера должно быть > 5 картинок
+    if len(images) < 6:
+        delta = 6 - len(images)
+        images += [SN(title="noimage", image=SN(url=f"{settings.MEDIA_URL}noimage.jpg"))]*delta
+
     return render(request, "index.html", {"images": images})
